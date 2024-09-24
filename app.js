ae7b45c4e3cbbd658aa3c0792926ada32dd0af57
Elvis Mudamba <elvismudamba@gail.com>
2024-09-24 16:38:17 +0300

    js update

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ let score;
 
 upper_bound_function()
 
+
 function submit_btn_handler(){
 
     submit_btn.addEventListener("click", ()=>{
@@ -74,52 +75,57 @@ function main_game(){
        
     })
 }
-function play_game(){
-
-   
-    check_btn.addEventListener("click", ()=>{
+let nextButtonCreated = false;
+let nextbtn;
+function play_game() {
+    check_btn.addEventListener("click", () => {
         input_box = document.querySelector(".input-box").value;
 
-        if(input_box != ''){
-            input_box = Number(input_box)
+        if (input_box !== '') {
+            input_box = Number(input_box);
 
-        if(input_box == computer_random_number){
-            trials++;
-            verdict.textContent = "you got it !!"
-            console.log("you got it")
-            score.textContent ="Score: " + trials;
-            
-            check_btn.style.display = "none"
-            let nextbtn = document.createElement("button")
-            nextbtn.classList.add("check-btn")
-            nextbtn.innerHTML = 
-            `
-            Next
-            `
-            main_game_container.appendChild(nextbtn)
-            nextbtn.addEventListener("click", ()=>{
-                console.log("clicked")
-                
-                upper_bound_function()
-                
-            })
-        }
-        else{
-            if(input_box> computer_random_number){
-                verdict.textContent = "you are above"
-                console.log("you are above")
+            if (input_box === computer_random_number) {
+                trials++;
+                verdict.textContent = "You got it !!";
+                console.log("You got it");
+                score.textContent = "Score: " + trials;
+
+                check_btn.style.display = "none";
+
+               
+                if (!nextButtonCreated) {
+                    
+                    nextbtn = document.createElement("button");
+                    nextbtn.classList.add("next-btn", "get-started-btn");
+                    nextbtn.innerHTML = "Next";
+                    main_game_container.appendChild(nextbtn);
+                    nextButtonCreated = true; 
+                   
+                    nextbtn.addEventListener("click", reset_game);
+                    
+                }
+            } else {
+                verdict.textContent = input_box > computer_random_number ? "You are above" : "You are below";
+                console.log(input_box > computer_random_number ? "You are above" : "You are below");
             }
-            else{
-                 verdict.textContent = "you are below"
-                
-                console.log("you are bleow")
-            }
+        } else {
+            verdict.textContent = "Please enter your guess";
         }
-        }
-        else{
-            verdict.textContent = "please enter your guess"
-        }
-        
-    })    
+    });
+}
+
+function reset_game() {
+
+     nextbtn.style.display = "none"
+ 
+    document.querySelector(".input-box").value = '';
+    verdict.textContent = '';
     
+ 
+    check_btn.style.display = "inline";
+    
+
+    computer_random_number = Math.floor(Math.random() * upper_bound_input_box) + 1;
+    console.log("New random number = ", computer_random_number)
+    nextButtonCreated = false;
 }
